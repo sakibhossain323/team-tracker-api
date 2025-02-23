892e74c64398e25b0fb694109801c8daa4e744a1
sakibhossain323 <sakibhossain323@gmail.com>
2025-02-23 19:37:29 +0600

    feat(teams): add endpoints

--- src/presentation/routes/teams.ts
+++ src/presentation/routes/teams.ts
@@ -2,14 +2,34 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
     console.log(req.headers);
+    console.log(req.query);
+    res.status(200).send([
+        { id: 1, name: "team1" },
+        { id: 2, name: "team2" },
+    ]);
+});
+
+router.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    res.status(200).send({ id: id, name: "team1" });
+});
+
+router.post("/", (req, res) => {
     console.log(req.body);
     res.status(201).send({ message: "Created!" });
 });
-router.get("/", (req, res) => {
-    console.log(req.headers);
-    res.send({ message: "Hello World!" });
+
+router.patch("/:id", (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    res.status(200).send({ message: "Updated!" });
+});
+
+router.delete("/:id", (req, res) => {
+    console.log(req.params);
+    res.status(204).send();
 });
 
 export default router;
