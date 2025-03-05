6ca0cc0e6c276f5f8507ddad00ff9462e5128fee
sakibhossain323 <sakibhossain323@gmail.com>
2025-03-05 21:23:00 +0600

    chore: remove unused method

--- src/infrastructure/repositories/membershipsRepository.ts
+++ src/infrastructure/repositories/membershipsRepository.ts
@@ -1,18 +1,5 @@
 import prisma from "../prisma/client";
 
-export async function hasMembership(userId: number, teamId: number) {
-    const membership = await prisma.membership.findUnique({
-        where: {
-            userId_teamId: {
-                userId,
-                teamId,
-            },
-        },
-    });
-
-    return membership !== null;
-}
-
 const findByUserIdAndTeamId = async (userId: number, teamId: number) => {
     return await prisma.membership.findUnique({
         where: {
